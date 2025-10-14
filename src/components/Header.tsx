af2c43c01f556850ed1b014a6304b9d208600642
SatwikBaramal <satwikbaramal854@gmail.com>
2025-10-14 17:31:15 +0530

    Changed Header Name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
-      <h1 className="text-2xl font-bold">Budget Tracking App</h1>
+    <header className="bg-gray-800 text-white p-4 flex justify-around items-center">
+      <h1 className="text-2xl font-bold">Vivaranam</h1>
       <p className="font-[var(--font-crimson-text)] text-sm">Made by Satwik Baramal</p>
     </header>
   );
